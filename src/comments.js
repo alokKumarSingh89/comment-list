7bb9f77aa33e3734ab7e7c5e78da42655d617d18
alok.singh <apple@applesmacbook.local>
2022-04-30 20:49:15 -0700

    added comment pull function

---
+++ src/comments.js
@@ -0,0 +1,65 @@
+const moment = require('moment')
+const { http } = require('./axiosHttp')
+const { log } = require('./utils')
+async function pullComment(repo, period, type = '', page = 0) {
+  const data = {}
+  let flag = false
+  try {
+    log('*', ' ', 5)
+    const url = repo.replace('PAGE', page)
+    const res = await http.get(url)
+    if (res.data.length <= 0) {
+      return data
+    }
+    res.data.forEach((item) => {
+      if (
+        period > -1 &&
+        !moment().subtract(period, 'days').isBefore(moment(item.created_at))
+      ) {
+        if (type !== 'commit') {
+          flag = true
+        }
+        return
+      }
+      if (data[item.user.login]) {
+        data[item.user.login] = {
+          comment: item.body
+            ? data[item.user.login].comment + 1
+            : data[item.user.login].comment,
+          commit: item.commit_id
+            ? data[item.user.login].comment + 1
+            : data[item.user.login].comment,
+        }
+      } else {
+        data[item.user.login] = {
+          comment: item.body ? 1 : 0,
+          commit: item.commit_id ? 1 : 0,
+        }
+      }
+    })
+    let newData = {}
+    if (!flag) {
+      newData = await pullComment(repo, period, type, page + 1)
+    }
+    return merge(newData, data)
+  } catch (e) {
+    return data
+  }
+}
+function merge(from, to) {
+  for (let key in from) {
+    if (to[key]) {
+      to[key] = {
+        comment: to[key].comment + from[key].comment,
+        commit: to[key].commit + from[key].commit,
+      }
+    } else {
+      to[key] = from[key]
+    }
+  }
+  return to
+}
+module.exports = {
+  pullComment,
+  merge,
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,48 @@
 const chalk = require('chalk')
+const minimist = require('minimist')
+
 const config = require('./config')
+const { pullComment, merge } = require('./comments')
+const { log, print } = require('./utils')
 
-console.log(chalk.yellow('Your github token is:'))
-console.info(chalk.yellow(config.GITHUB_PERSONAL_ACCESS_TOKEN))
+async function start() {
+  const args = minimist(process.argv.slice(2))
+  console.log(args)
+  let messages = '\n    Fetching comments'
+  const repo = args._[0] || args['repo']
+  if (!repo) {
+    throw new Error('Repo is required')
+  }
 
-// remove this line
-require('./example')
+  let period = parseInt(args._[1] || args['period'] || '-1', 10)
+  if (period >= 1) {
+    messages += ` ${chalk.blue('for')} past ${period} days`
+  }
+  messages += ` ${chalk.blue('for')} ${chalk.yellowBright(`"${repo}"`)}...\n\n`
+  log(messages)
+  log('    ')
+  let COMMIT_URL = config.COMMIT_URL.replace(/REPOSITORY/, repo)
+  let PULL_URL = config.PULL_URL.replace(/REPOSITORY/, repo)
+  let ISSUE_URL = config.ISSUE_URL.replace(/REPOSITORY/, repo)
+  Promise.all([
+    pullComment(COMMIT_URL, period, 'commit'),
+    pullComment(PULL_URL, period),
+    pullComment(ISSUE_URL, period),
+  ])
+    .then((res) => {
+      let data = {}
+      data = merge(res[0], data)
+      if (res[1]) {
+        data = merge(res[1], data)
+      }
+      if (res[2]) {
+        data = merge(res[2], data)
+      }
+      print(data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+start()
