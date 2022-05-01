2309bf3c1d5f51cb65f558d23548c06665ed14b4
alok.singh <apple@applesmacbook.local>
2022-04-30 20:47:53 -0700

    Updated comment url and  create  util function for comment

--- src/config.js
+++ src/config.js
@@ -1,3 +1,8 @@
 module.exports = {
   GITHUB_PERSONAL_ACCESS_TOKEN: require('./token'),
+  COMMIT_URL: '/repos/REPOSITORY/comments?page=PAGE',
+  PULL_URL:
+    '/repos/REPOSITORY/pulls/comments?page=PAGE&sort=created_at&direction=desc',
+  ISSUE_URL:
+    '/repos/REPOSITORY/issues/comments?page=PAGE&sort=created_at&direction=desc',
 }
---
+++ src/utils.js
@@ -0,0 +1,39 @@
+const leftPad = require('left-pad')
+function log(message) {
+  process.stdout.write(message)
+}
+function sortBasedonCommnetAndReturnArray(data) {
+  const arr = []
+  for (let key in data) {
+    arr.push({
+      name: key,
+      commit: data[key].commit,
+      comment: data[key].comment,
+    })
+  }
+  arr.sort((b, a) =>
+    a.comment > b.comment ? 1 : b.comment > a.comment ? -1 : 0,
+  )
+
+  return arr
+}
+function print(data) {
+  if (Object.keys(data).length <= 0) {
+    console.log('\n')
+    console.log('    No Comments')
+  }
+  let sortedData = sortBasedonCommnetAndReturnArray(data)
+  console.log('\n')
+  sortedData.forEach((obj) => {
+    console.log(
+      `${leftPad(obj.comment, 4)} comments, ${obj.name} (${
+        obj.commit
+      } commits)`,
+    )
+  })
+}
+
+module.exports = {
+  print,
+  log,
+}
